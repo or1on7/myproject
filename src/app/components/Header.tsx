5fa79440bd96902920447906db6319b7f1c0e783
or1on7 <orion7007@gmail.com>
2023-11-19 16:31:47 +0100

    Commit #2

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,22 @@
 import Link from "next/link";
-import styles from '../styles/Header.module.css'
+//import styles from '../styles/Header.module.css'
 
 export default function Header(){
     return (
-        <>
-            <ul className={styles.menu}>
-                <li><Link href="/">Accueil</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-                <li><Link href="/dog">Dog</Link></li>
-                <li><Link href="/music">Music</Link></li>
-                <li><Link href="/profile/1">Profile</Link></li>
-            </ul>
-        </>
-    )
+        <nav className="bg-gray-800 p-4">
+          <div className="container mx-auto flex items-center justify-between">
+            <div className="flex items-center space-x-4">
+              <Link href="/" className="text-white">Accueil</Link>
+              <Link href="/contact" className="text-white">Contact</Link>
+              <Link href="/dog" className="text-white">Dog</Link>
+              <Link href="/music" className="text-white">Music</Link>
+              <Link href="/profile/1" className="text-white">Profile</Link>
+            </div>
+            {/* Responsive Menu Button for Mobile */}
+            <div className="lg:hidden">
+              {/* Add your mobile menu button/icon here */}
+            </div>
+          </div>
+        </nav>
+      )
 }
